0f01ab341b1b884c4b4f5af1ea836768beb8b545
mandorlinoGitHub <alejandroalmendro@gmail.com>
2019-10-11 12:04:43 +0200

    terminado ejercicio variables

--- práctica var const y For/ConsoleAppAA var const y For/ConsoleApp1/Program.cs
+++ práctica var const y For/ConsoleAppAA var const y For/ConsoleApp1/Program.cs
@@ -7,36 +7,64 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             // fuente: https://drive.google.com/file/d/1z9pZhZDjiV5D5GHdG3rKZ-_jd1GAXMtp/view
+
             #region FASE 1: Inicializar y mostrar variables int y string.
             string name = "Elijendru";
             string lastName1 = "Elmindru";
             string lastName2 = "Difodilobas";
 
-            int day = 8;
-            int month = 6;
-            int year = 1911;
+            int birthDay = 8;
+            int birthMonth = 6;
+            int birthYear = 1986;
 
             Console.WriteLine(name + " " + lastName1 + " " + lastName2);
-            Console.WriteLine(day + " / " + month + " / " + year);
+            Console.WriteLine(birthDay + " / " + birthMonth + " / " + birthYear);
+            Console.WriteLine();
             #endregion
 
-            #region FASE 2: Calcular años entre TRASPAS y nacimiento.
-            const int TRASPAS = 1948;
+            #region FASE 2: Calcular años entre 1948 y nacimiento.
+            const int leapYear1948 = 1948;
+            const int leapYearPeriod = 4;
+            int ageDifference = birthYear - leapYear1948;
+            int countLeapYears = ageDifference / leapYearPeriod;
 
-            int ageDifference = year - TRASPAS;
-            if (ageDifference < 0) ageDifference= ageDifference *-1;
-
-            Console.WriteLine(ageDifference);
-            //TODO falta una variable
+            Console.WriteLine(countLeapYears);
+            Console.WriteLine();
             #endregion
 
-            #region FASE 3:
-            //TODO
+            #region FASE 3: Listar años bisiestos y determinar si el año de nacimiento lo es.  
+            string isLeapYear = "El año de nacimiento es bisiesto.";
+            string isNotLeapYear = "El año de nacimiento no es bisiesto.";
+            bool is_BD_LY_check;
+
+            for (int i = leapYear1948;  i < birthYear;i += leapYearPeriod)
+            {
+                Console.WriteLine(i);
+            }
+
+            if (birthYear % leapYearPeriod == 0)
+                is_BD_LY_check = true;
+            else
+                is_BD_LY_check = false;
+
+            if (is_BD_LY_check == true)
+                Console.WriteLine(isLeapYear);
+            else
+                Console.WriteLine(isNotLeapYear);
+            Console.WriteLine();
             #endregion
 
-            #region FASE 4:
-            //TODO
-            #endregion
+            #region FASE 4: Imprimir toda la info.
+            string fullName = $"{name} {lastName1} {lastName2}";
+            string fullDate = $"{birthDay} / {birthMonth} / {birthYear}";
+
+            Console.WriteLine(fullName);
+            Console.WriteLine(fullDate);
+            if (is_BD_LY_check == true)
+                Console.WriteLine(isLeapYear);
+            else
+                Console.WriteLine(isNotLeapYear);
+            #endregion 
         }
     }
 }
